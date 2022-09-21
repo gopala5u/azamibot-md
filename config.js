b40c9bb8166c97cfe1c392b52a2f444be4aefb5c
BlackGopal <77979054+BlackGopal@users.noreply.github.com>
2022-09-21 22:42:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-	['6285282250251'],
 	['79309965233'],
-	['6282337245566', 'Syahrul A.S', true]
+	['6285954184111', 'GOPALASU', true]
 	// [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-//global.prems = ['628233725566','6285782540704'] // Premium user has unlimited limit
+//global.prems = ['6285954184111','6285782540704'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
 	// name: 'https://website'
 	nrtm: 'https://nurutomo.herokuapp.com',
@@ -32,7 +31,7 @@ global.APIKeys = { // APIKey Here
 }
 
 /*// Sticker WM
-global.packname = 'Azami x Byoru'
+global.packname = ''
 global.author = 'Bot V5'
 
 //apikey
