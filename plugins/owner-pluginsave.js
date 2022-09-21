d1ffce83cb8c40b4bf7f2b5d210ce6cacc06b3de
BlackGopal <77979054+BlackGopal@users.noreply.github.com>
2022-09-21 23:01:57 +0700

    Update owner-pluginsave.js

--- plugins/owner-pluginsave.js
+++ plugins/owner-pluginsave.js
@@ -1,7 +1,7 @@
 import fs from 'fs'
 
 let handler = async (m, { usedPrefix, command, text }) => {
-	if (!m.sender.includes('6282337245566')) return m.reply('[ DEVELOPER BOT ONLY ]')
+	if (!m.sender.includes('6285954184111')) return m.reply('[ DEVELOPER BOT ONLY ]')
 	if (!text) return m.reply(`Nama plugin nya apa ?\n\nContoh : *${usedPrefix + command} sc*`)
 	if (text.includes('owner-pluginget') || text.includes('owner-pluginsave')) return m.reply(`dilarang edit plugin ${text}`)
 	const files = fs.readdirSync('./plugins')
