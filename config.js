014138eb9249dc6d89b96cfd15d055364be5f286
BlackGopal <77979054+BlackGopal@users.noreply.github.com>
2022-09-21 22:59:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-	['79309965233'],
+	['6285954184111'],
 	['6285954184111', 'GOPALASU', true]
 	// [number, dia creator/owner?, dia developer?]
 ] // Put your number here
